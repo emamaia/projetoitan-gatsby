1b128aa2c05b534a354c7d2f2d01c02f347da323
ema_maia@hotmail.com <emamaia@hotmail.com>
2020-01-29 15:35:58 -0300

    Updates

---
+++ precache-manifest.7c545ab2c2e78535a2c224a2adff7e00.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ad38ed9bbfc082cd022c910ceb3c8cc",
+    "url": "/projeto-Itan/index.html"
+  },
+  {
+    "revision": "89a0f9d155b33e6d1ff8",
+    "url": "/projeto-Itan/static/css/main.d5d22a7f.chunk.css"
+  },
+  {
+    "revision": "47c71abac6a0dbf4d4b6",
+    "url": "/projeto-Itan/static/js/2.9e012704.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/projeto-Itan/static/js/2.9e012704.chunk.js.LICENSE"
+  },
+  {
+    "revision": "89a0f9d155b33e6d1ff8",
+    "url": "/projeto-Itan/static/js/main.833d2361.chunk.js"
+  },
+  {
+    "revision": "67dfb3a7c6f9506c348a",
+    "url": "/projeto-Itan/static/js/runtime-main.2257c15d.js"
+  },
+  {
+    "revision": "31d5efadbfd657d55765194e0d323103",
+    "url": "/projeto-Itan/static/media/logo.31d5efad.png"
+  },
+  {
+    "revision": "3a9281b1c391a5828f780d920ae8373c",
+    "url": "/projeto-Itan/static/media/logo3.3a9281b1.png"
+  },
+  {
+    "revision": "66b8097f292c90fd9f1a8494b58465de",
+    "url": "/projeto-Itan/static/media/mapa1.66b8097f.png"
+  },
+  {
+    "revision": "3fcc53a5fdfd9823ee43a63650084293",
+    "url": "/projeto-Itan/static/media/principal.3fcc53a5.png"
+  }
+]);
